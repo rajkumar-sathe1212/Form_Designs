187735f1640ee4dce0eb90a42d0525a24a0c8221
Rajkumar Sathe <satheraj1400@gmail.com>
2023-02-19 13:57:07 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { Form1Component } from './form1/form1.component';
 import { Form2Component } from './form2/form2.component';
 import { Form3Component } from './form3/form3.component';
+import { Form4Component } from './form4/form4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Form1Component,
     Form2Component,
-    Form3Component
+    Form3Component,
+    Form4Component
   ],
   imports: [
     BrowserModule,
